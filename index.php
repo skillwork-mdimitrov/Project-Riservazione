8bdd0f446962d8ec8a74c5540fe5f170f504e5c3
Maksim <Maksim@Maksim-PC>
2016-11-23 19:56:00 +0100

    - CSS folder for storing CSS files - Images folder for storing images - Header section (not finished)

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> <!-- so the browsers can use the latest rendering standards. -->
+<html>
+    <head>
+        <meta charset="UTF-8"/>  <!-- allows many more languages to be mixed on a single page than any other choice of encoding. -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> <!-- to support IE 9 or IE 8 -->
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- render the width of the page at the width of its own screen, So if that screen
+        is 320px wide, the browser window will be 320px wide -->
+        <link rel="stylesheet" href="CSS/indexCSS.css">
+        <title></title>
+    </head>
+</html>

8b559b597f7a580c234ca9cbd9c779fddcab5084
user120310 <user120310@hotmail.com>
2016-11-24 12:25:45 +0100

    body of web

--- index.php
+++ index.php
@@ -6,6 +6,98 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- render the width of the page at the width of its own screen, So if that screen
         is 320px wide, the browser window will be 320px wide -->
         <link rel="stylesheet" href="CSS/indexCSS.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
         <title></title>
     </head>
+    <body>
+
+        <!--nav bar-->
+        <div class="row">
+            <div class="col-sm-3"><p>home</p></div>
+            <div class="col-sm-7"><p>nothing</p></div>
+            <div class="col-sm-2"><p>login</p></div>
+        </div>
+        
+        <!--nav bar but for the room-->
+        <div class="row">
+            <div class="col-sm-4"><p>room</p></div>
+            <div class="col-sm-4"><p>nothing</p></div>
+            <div class="col-sm-4"><p>week</p></div>
+        </div>
+        
+        <!--table 1-->
+        <div class="row">
+            <div class="col-sm-12"><p>Room name</p></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-12"><p>table</p></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8"><p>nothing</p></div>
+            <div class="col-sm-4"><p>booking</p></div>
+        </div>
+        
+        <!--table 2-->
+        <div class="row">
+            <div class="col-sm-12"><p>Room name</p></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-12"><p>table</p></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8"><p>nothing</p></div>
+            <div class="col-sm-4"><p>booking</p></div>
+        </div>
+        
+        <!--table 3-->
+        <div class="row">
+            <div class="col-sm-12"><p>Room name</p></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-12"><p>table</p></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8"><p>nothing</p></div>
+            <div class="col-sm-4"><p>booking</p></div>
+        </div>
+        
+        <!--table 4-->
+        <div class="row">
+            <div class="col-sm-12"><p>Room name</p></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-12"><p>table</p></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8"><p>nothing</p></div>
+            <div class="col-sm-4"><p>booking</p></div>
+        </div>
+        
+        <!--table 5-->
+        <div class="row">
+            <div class="col-sm-12"><p>Room name</p></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-12"><p>table</p></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-8"><p>nothing</p></div>
+            <div class="col-sm-4"><p>booking</p></div>
+        </div>
+
+  
+        <!--footer-->
+  <footer class="container-fluid text-center">
+    <p>Footer Text</p>
+  </footer>
+
+  </body>
 </html>

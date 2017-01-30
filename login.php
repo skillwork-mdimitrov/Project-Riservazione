ea777c945f660bd040424b6d1614117b920148e5
Maksim <Maksim@Maksim-PC>
2017-01-30 14:02:07 +0100

    - push everything

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include 'dbConnectionVM.php';
+include 'dbConnection.php';
 $adminAccess = 0; // everyone is treated as normal user, unless he's granted admin access
 $CheckingString = "SELECT userNumber, userEmail FROM $UserTable WHERE userEmail='$userEmail' AND userPasswordSalt='".substr(md5($userPw), 0, 20)."'";
 $CheckingQuery = mysqli_query($DBConnect, $CheckingString) ;
